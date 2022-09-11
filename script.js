e832f4e98a85c7432abb0da1ae220c455b64cd1c
amartyagunjan <amartyagunjan01@gmail.com>
2022-09-11 10:05:45 +0530

    Adding intial files

---
+++ script.js
@@ -0,0 +1,10 @@
+let addBtn = document.querySelector(".add-btn");
+let addFlag = false;
+let modalBox = document.
+
+addBtn.addEventListener("click", (event) =>{
+    if(addFlag == false){
+        addFlag = true;
+
+    }
+} )
